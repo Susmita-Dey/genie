98e739be964c65f465d04c9df8c56d23b140038e
Susmita-Dey <susmitadey475@gmail.com>
2025-04-28 21:16:11 +0530

    add google fonts package and complete sidebar UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:genie/pages/home_page.dart';
 import 'package:genie/theme/colors.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,8 +13,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(scaffoldBackgroundColor: AppColors.background),
+      title: 'Genie',
+      theme: ThemeData(
+        scaffoldBackgroundColor: AppColors.background,
+        textTheme: GoogleFonts.interTextTheme(ThemeData.dark().textTheme),
+      ),
       home: const HomePage(),
     );
   }
