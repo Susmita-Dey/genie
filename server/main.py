2b0928d01a919604018128c7e1b599d442162fbb
Susmita-Dey <susmitadey475@gmail.com>
2025-04-30 22:30:50 +0530

    Implement chat endpoint and search service integration

--- server/main.py
+++ server/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from pydantic_models.chat_body import ChatBody
+from service.search_service import SearchService
 
 app = FastAPI()
+search_service = SearchService()
 
 
-@app.get("/")
-def hello_world():
-    return "Hello World"
+# chat
+@app.get("/chat")
+def chat_endpoint(body: ChatBody):
+    # search the web and find appropriate sources
+    search_service.web_search(body.query)
+    # sort the sources
+    # generate the response using LLM
+    return body.query
---
+++ server/service/search_service.py
@@ -0,0 +1,11 @@
+from config import Settings
+from tavily import TavilyClient
+
+settings = Settings()
+tavily_client = TavilyClient(api_key=settings.TAVILY_API_KEY)
+
+
+class SearchService:
+    def web_search(self, query: str):
+        response = tavily_client.search(query, max_results=1)
+        print(response)
